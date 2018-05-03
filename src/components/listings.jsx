0a7ce55ad42c9dcf209404829803753b0fce2178
Corey Ladovsky <coreyskymusic@gmail.com>
2018-05-03 18:47:48 -0400

    filter cutting down array but not updating component

--- src/components/listings.jsx
+++ src/components/listings.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
-import ListingItem from './listingItem';
+import React from "react";
+import ListingItem from "./listingItem";
 
 class Listings extends React.Component {
-  componentDidMount() {
-      this.props.fetchListings();
+  constructor(props) {
+    super(props);
+    this.checkFilter = this.checkFilter.bind(this);
+    this.makeList = this.makeList.bind(this);
+    this.state = {filterLength: 0};
   }
 
-  render () {
-    const listings = this.props.listings.map(listing => {
-      return <ListingItem key={listing.addressID} listing={listing}/>;
+  componentDidMount() {
+    this.props.fetchListings();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.props.filters.length !== nextProps.filters.length) {
+      this.setState({filterLength: nextProps.filters.length});
+    }
+  }
+
+  makeList() {
+    // let listings;
+    // if (this.state.filterLength !== 0) {
+    //   listings = [];
+    //   this.props.listings.forEach(listing => {
+    //     if (this.checkFilter(listing)) {
+    //       listings.push(listing);
+    //     }
+    //   });
+    // } else {
+      let listings = this.props.listings;
+      debugger
+    // }
+
+    return listings.map(listing => {
+      return <ListingItem key={listing.addressID} listing={listing} />;
     });
-    return(
-      <ul>
-        {listings}
-      </ul>
-    );
+  }
+
+  checkFilter(listing) {
+    if(!listing.amenities) {
+      return false;
+    }
+
+    for (let i = 0; i < this.props.filters.length; i++) {
+      if(!listing.amenities.includes(this.props.filters[i]) ){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  render() {
+    return <ul>{this.makeList()}</ul>;
   }
 }
 
--- src/components/listingsContainer.jsx
+++ src/components/listingsContainer.jsx
@@ -2,10 +2,43 @@ import { connect } from 'react-redux';
 import { fetchListings } from '../actions/listingActions';
 import Listings from './listings';
 
+const checkFilter = (listing, filters) => {
+  if(!listing.amenities) {
+    return false;
+  }
+
+  for (let i = 0; i < filters.length; i++) {
+    if(!listing.amenities.includes(filters[i]) ){
+      return false;
+    }
+  }
+  return true;
+};
 
 const mapStateToProps = (state, ownProps) => {
+  let filters = [];
+   Object.keys(state.filters).forEach( key => {
+     if(state.filters[key]) {
+       filters.push(key);
+     }
+   });
+
+
+   let listings = Object.values(state.listings);
+   if (filters.length !== 0) {
+     let newlist = [];
+     listings.forEach(listing => {
+       if (checkFilter(listing, filters)) {
+         newlist.push(listing);
+       }
+     });
+     listings = newlist;
+   }
+
+
   return {
-    listings: Object.values(state.listings)
+    listings,
+    filters
   };
 };
 
