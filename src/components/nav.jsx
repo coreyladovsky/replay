fa1b3cbf47bb4535e05c536d30844a192a3d870f
Corey Ladovsky <coreyskymusic@gmail.com>
2018-05-03 13:27:39 -0400

    minor change

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import logo from "../images/replay_logo.png";
 import "../css/nav.css";
+import { baseUrl } from '../index.js';
+
 
 class NavBar extends React.Component {
   render() {
     return (
       <div className="navContainer">
         <div className="navContainerLogo">
-          <Link to={"/"}>
+          <Link to={baseUrl + "/"}>
           <img src={logo} alt="" />
         </Link>
         </div>
         <div className="AllListings" >
-          <Link to={"/"}>All Listings</Link>
+          <Link to={baseUrl + "/"}>All Listings</Link>
         </div>
       </div>
     );
